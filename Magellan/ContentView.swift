fa087c21a23c358a474b9eacfaa56872e55dfb35
Quinn McHenry <quinn@jqm.us>
2021-06-12 20:18:28 -0400

    Initial Commit

---
+++ Magellan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Magellan
+//
+//  Created by Quinn McHenry on 6/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Magellan/MagellanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagellanApp.swift
+//  Magellan
+//
+//  Created by Quinn McHenry on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MagellanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
